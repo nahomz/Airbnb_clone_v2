810f7d3d78fe314ab74e23380f24f74d9671f811
Getinet <getinet.dtu@outlook.com>
2022-03-09 09:16:12 +0300

    adding setup_mysql_dev.sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
--- Create dev database and user
+-- Script that prepares a MySQL server for the project:
+-- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
